38d8b7c87ff9ed12f5d14919702d8deeaac05522
Liaaaaaaaaaa1 <gomezligia296@gmail.com>
2025-11-02 23:39:16 -0500

    Reorganice la estructura del proyecto

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EstadisticasPersonales from "./components/EstadisticasPersonales";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GameTracker Dashboard</h1>
+      <EstadisticasPersonales />
+    </div>
+  );
+}
+
+export default App;
